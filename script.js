70a9f68072f99bc0c82fa7c5c76cf3d2c47057ad
DaveBett <davide.betterini@gmail.com>
2026-06-30 14:26:59 +0100

    Updated Collision check and general logic of the game

--- script.js
+++ script.js
@@ -1,65 +1,88 @@
 const character = document.getElementById("character");
 const obstacle = document.getElementById("obstacle");
 const scoreDisplay = document.getElementById("score");
+const sizes = ["small", "medium", "large"];
 let speed = 1500;
 let score = 0;
 
-const generateObstacle = setInterval(function() {
-  let size = Math.floor(Math.random() * 3) + 1;
-  switch(size) {
-    default:
-    case 1:
-      obstacle.className = "small";
-      break;
-    case 2:
-      obstacle.className = "mid";
-      break;
-    case 3:
-      obstacle.className = "large";
-      break;
-  }
-}, speed);
 
-const speedUp = function() {
-  if (score >= 1000) {
-    speed = 500;
-    obstacle.style.animation = "slide 0.5s infinite linear";
-  } else if (score >= 750) {
-    speed = 750;
-    obstacle.style.animation = "slide 0.75s infinite linear";
-  } else if (score >= 500) {
-    speed = 1000;
-    obstacle.style.animation = "slide 1s infinite linear";
-  } else if (score >= 250) {
-    speed = 1250;
-    obstacle.style.animation = "slide 1.25s infinite linear";
-  } 
+obstacle.style.animation = `slide ${speed.toString()}ms infinite linear`;
+
+function getRandomObstacleSize() {
+    i = Math.floor(Math.random() * (sizes.length));
+    return sizes[i];
 }
 
-const scoreInterval = setInterval(() => {
-  score += 1;
-  scoreDisplay.textContent = `Score: ${score}`;
-}, 50);
+function randomizeSize() {
+  setInterval(() => {
+    const size = getRandomObstacleSize();
+    obstacle.className = `${size}`
+  }, speed)
+}
 
-const checkGameOver = setInterval(function() {
-  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-  let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+//function speedUp() {
+//  setInterval(function() {
+//    if (score >= 1000) {speed = 500;}
+//    else if (score >= 750) {speed = 750;}
+//    else if (score >= 500) {speed = 1000;} 
+//    else if (score >= 250) {speed = 1250;} 
+//
+//    obstacle.style.animation = `slide ${speed.toString()}ms infinite linear`;
+//    console.log(obstacle.style.animation);
+//  }, speed);
+//}
 
-  if(obstacleLeft < 20 && obstacleLeft > 0 && characterTop >= 130) {
-    obstacle.style.animation = "none";
-    obstacle.style.display = "none";
-    alert (`Game Over\nYour Score: ${score}`);
-    window.location.reload();
-  };
-}, 10);
+function scoreUp() {
+  setInterval(() => {
+    score += 1;
+    scoreDisplay.textContent = `Score: ${score}`;
+  }, 100);
+}
 
-document.addEventListener('keydown', (e) => {
-  if (e.code = "Space") {
-    if (character.classList !== "jump") {
-      character.classList.add("jump");
+function checkGameOver() {
+  const characterClientRect = character.getBoundingClientRect();
+  const characterL = characterClientRect.left;
+  const characterR = characterClientRect.right;
+  const characterB = characterClientRect.bottom;
+
+  const obstacleClientRect = obstacle.getBoundingClientRect();
+  const obstacleL = obstacleClientRect.left;
+  const obstacleR = obstacleClientRect.right;
+  const obstacleT = obstacleClientRect.top;  
+
+  const xCollision = obstacleR > characterL && obstacleL < characterR;
+  const yCollision = characterB > obstacleT;
+  
+  return xCollision && yCollision;
+}
+
+function monitorCollision() {
+  setInterval(() => {
+    if (checkGameOver()) {
+      alert(`Game Over.\nYour Score: ${score}`);
     }
-    setTimeout(function() {
-      character.classList.remove("jump");
-    }, 500);
-  }
-});
+  }, 10);
+}
+
+function jump() {
+  document.addEventListener('keydown', (e) => {
+    if (e.code == "Space") {
+      if (character.classList !== "jump") {
+        character.classList.add("jump");
+      }
+      setTimeout(function() {
+        character.classList.remove("jump");
+      }, 500);
+    }
+  });
+}
+
+function game() {
+  jump();
+  randomizeSize();
+  scoreUp();
+  //speedUp();
+  //monitorCollision();
+}
+
+game();

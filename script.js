f6c494d5de49f6aeecef5df967d305d4862dc287
DaveBett <davide.betterini@gmail.com>
2026-06-30 11:39:49 +0100

    Added scoring system

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let character = document.getElementById("character");
-let obstacle = document.getElementById("obstacle");
+const character = document.getElementById("character");
+const obstacle = document.getElementById("obstacle");
+const scoreDisplay = document.getElementById("score");
+let score = 0;
 
-let generateObstacle = setInterval(function() {
+const generateObstacle = setInterval(function() {
   let size = Math.floor(Math.random() * 3) + 1;
   switch(size) {
     default:
@@ -17,14 +19,19 @@ let generateObstacle = setInterval(function() {
   }
 }, 1500);
 
-let checkGameOver = setInterval(function() {
+const scoreInterval = setInterval(() => {
+  score += 1;
+  scoreDisplay.textContent = `Score: ${score}`;
+}, 100);
+
+const checkGameOver = setInterval(function() {
   let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
   let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
 
   if(obstacleLeft < 20 && obstacleLeft > 0 && characterTop >= 130) {
     obstacle.style.animation = "none";
     obstacle.style.display = "none";
-    alert ('Game Over');
+    alert (`Game Over\nYour Score: ${score}`);
     window.location.reload();
   };
 }, 10);

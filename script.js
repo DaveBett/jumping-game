63678be21132e0cf1647779458e32d5275936178
DaveBett <davide.betterini@gmail.com>
2026-06-30 10:40:08 +0100

    added Game Over check

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 let character = document.getElementById("character");
 let obstacle = document.getElementById("obstacle");
 
-console.log(character);
-
-document.addEventListener('keydown', (e) =>{
+document.addEventListener('keydown', (e) => {
   if (e.code = "Space") {
     if (character.classList !== "animate") {
       character.classList.add("animate");
@@ -13,3 +11,14 @@ document.addEventListener('keydown', (e) =>{
     }, 500);
   }
 });
+
+let checkGameOver = setInterval(function() {
+  let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+
+  let obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue("left"));
+  if(obstacleLeft < 20 && obstacleLeft > 0 && characterTop > 130) {
+    obstacle.style.animation = "none";
+    obstacle.style.display = "none";
+    alert ('Game Over');
+  };
+}, 10);
